9202c133374dd78f3554d6ce863745c231b894c2
dfworkroom01 <dfworkroom01@gmail.com>
2024-07-12 21:15:35 +0800

    Change the first element in the array

--- numbertostring.js
+++ numbertostring.js
@@ -3,11 +3,15 @@ function replaceNumWithStringInArray(arr, n, str) {
       if (arr[i] === n) {
         arr[i] = str;
       }
+       
     }
    }
   
   let a = ['cat', 4,'mouse', 'giraffe', 4]; 
   console.log('before', a);
 
-  replaceNumWithStringInArray(a, 4, 'dog');
+  replaceNumWithStringInArray(a, 4, 'dog');  
   console.log('after', a);
+
+    a.splice(0,1, 'turtle')
+    console.log('after changing all numbers', a);
