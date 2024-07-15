06e92d2a8ef4a8941937ba4a1db9aaa76838e90b
dfworkroom01 <dfworkroom01@gmail.com>
2024-07-15 22:51:07 +0800

    Add explanation per code

--- numbertostring.js
+++ numbertostring.js
@@ -1,3 +1,5 @@
+//case: we have a string with a mix of string and number at the index of 1 and 4 of the array
+//first, replace all numbers with strings
 function replaceNumWithStringInArray(arr, n, str) {
     for (let i = 0; i < arr.length; ++i) {
       if (arr[i] === n) {
@@ -6,15 +8,17 @@ function replaceNumWithStringInArray(arr, n, str) {
        
     }
    }
-  
+//the original array with a mix of numbers and strings
   let a = ['cat', 4,'mouse', 'giraffe', 4]; 
   console.log('before', a);
-
+//To replace numbers at index 1 and 4 of the array with a string
   replaceNumWithStringInArray(a, 4, 'dog');  
   console.log('after', a);
 
+  //extra: changing the zero iondex in the array, which was 'cat', with ;turtle' 
     a.splice(0,1, 'turtle')
     console.log('after changing all numbers', a);
-
+   
+  //remove the 4th index position, which was 'dog' from the array
     removed = a.splice(4, 1);
     console.log('remove the last element', a);
